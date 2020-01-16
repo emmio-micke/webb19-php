b7b46cd405cb79bd686eb33069520fbf87474646
Mikael Olsson <mikael.olsson@emmio.se>
2020-01-16 14:07:13 +0100

    Example 06 - send and receive form.

---
+++ examples/06-form/send.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Send form</title>
+</head>
+<body>
+    <h1>Send form</h1>
+    <form method="post" action="receive.php">
+        Name:<br>
+        <input type="text" name="my_name"><br>
+        Age:<br>
+        <input type="text" name="age"><br>
+        <input type="submit" name="send">
+    </form>
+</body>
+</html>
