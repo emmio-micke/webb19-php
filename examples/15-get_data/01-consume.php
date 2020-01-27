af4059a886b2c371431210910c85a89c1cbd080d
Mikael Olsson <mikael.olsson@emmio.se>
2020-01-27 09:27:58 +0100

    Solution suggestion 15 - api consumer

--- examples/15-get_data/01-consume.php
+++ examples/15-get_data/01-consume.php
@@ -4,7 +4,7 @@
 $url = 'http://api.icndb.com/jokes/random';
 $file_content = file_get_contents($url);
 
-// var_dump($file_content);
+var_dump($file_content);
 
 $value = json_decode($file_content);
 
---
+++ examples/15-get_data/01-consume2.php
@@ -0,0 +1,49 @@
+<?php
+
+$url = 'https://randomuser.me/api/?results=5';
+$file_content = file_get_contents($url);
+
+$people = json_decode($file_content);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>List 5 people</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+<table class="table table-striped">
+    <thead class="thead-dark">
+        <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+<?php foreach ($people->results as $person) : ?>
+    <tr>
+        <td><?php
+            echo $person->name->first . ' ' . $person->name->first;
+        ?></td>
+
+        <td><?php
+            echo $person->location->street->name . ' ' . $person->location->street->number;
+        ?></td>
+
+        <td><?php
+            echo $person->email;
+        ?></td>
+
+    </tr>
+<?php endforeach; ?>
+</table>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
